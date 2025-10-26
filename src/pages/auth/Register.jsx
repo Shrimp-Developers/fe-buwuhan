ab1e45fd3b8288c625e233f080bff66677c3ee11
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-10-26 16:14:27 +0700

    refactor: change english to indonesia & source img

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -7,7 +7,7 @@ export default function Register() {
             {/* Left Side - Image Section */}
             <div className="flex items-center justify-center bg-[#8A86D5] w-full lg:w-1/2 h-72 lg:h-auto">
                 <img
-                    src="../public/logo.png"
+                    src="/logo.png"
                     alt="Deskripsi icon-buwuhan"
                     className="w-56 h-56 lg:w-[350px] lg:h-[350px] object-contain"
                 />
@@ -41,7 +41,7 @@ export default function Register() {
                                 <input
                                     type="password"
                                     name="password"
-                                    placeholder="Password"
+                                    placeholder="Kata Sandi"
                                     className="w-full pl-12 pr-12 py-3 bg-[#F5F5F5] rounded-xl focus:outline-none focus:ring-2 transition"
                                 />
                                 <button className="absolute inset-y-0 right-0 pr-4 flex items-center text-gray-400">
@@ -57,7 +57,7 @@ export default function Register() {
                                 <input
                                     type="password"
                                     name="confirmPassword"
-                                    placeholder="Confirm Password"
+                                    placeholder="Konfirmasi Kata Sandi"
                                     className="w-full pl-12 pr-12 py-3 bg-[#F5F5F5] rounded-xl focus:outline-none focus:ring-2 transition"
                                 />
                                 <button className="absolute inset-y-0 right-0 pr-4 flex items-center text-gray-400">
@@ -68,15 +68,15 @@ export default function Register() {
 
                         {/* Sign Up Button */}
                         <button className="w-full bg-[#000000] text-white py-3 rounded-xl font-semibold hover:bg-gray-800 transition shadow-lg">
-                            Sign Up
+                            Daftar
                         </button>
 
                         {/* Sign In Link */}
                         <div className="text-center">
                             <p className="text-sm text-gray-600">
-                                Already have an account?{' '}
+                                Sudah punya akun?{' '}
                                 <button className="text-[#8A86D5] font-semibold hover:underline">
-                                    Sign in
+                                    Masuk
                                 </button>
                             </p>
                         </div>
