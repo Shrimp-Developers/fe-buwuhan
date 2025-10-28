9e27e106b68a83d3cf21e9e524ea4c891c4cadcb
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-10-29 06:29:59 +0700

    feat: create CardProfile component file

---
+++ src/components/CardProfile.jsx
@@ -0,0 +1,11 @@
+export default function CardProfile({ children, className = '', colSpan = 1, height = 'h-32' }) {
+    return (
+        <div
+            className={`flex flex-col lg:flex-row lg:items-center lg:justify-between px-5 py-1 rounded-3xl shadow-sm border border-black ${height} ${
+                colSpan === 2 ? 'col-span-2' : colSpan === 3 ? 'col-span-3' : ''
+            } ${className}`}
+        >
+            {children}
+        </div>
+    );
+}
