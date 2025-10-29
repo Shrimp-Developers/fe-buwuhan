856b7def0f2ace969849a44afe2dadd35c3361ae
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-10-29 07:02:11 +0700

    style: change name comment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -81,7 +81,7 @@ export default function Sidebar({ isOpen, onClose }) {
 
                 </nav>
 
-                {/* Logout Button */}
+                {/* Logout */}
                 <div className="p-6 border-gray-300">
                     <Link to="/login" className="w-full flex items-center gap-3 px-4 py-3 text-gray-700 hover:bg-gray-300 rounded-lg text-sm transition-all duration-200">
                         <LogOut className="w-6 h-6 flex-shrink-0" />
