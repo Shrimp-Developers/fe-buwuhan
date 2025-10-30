cad27e7438c0cd2c64acbe9e4f72a1b061b90660
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-10-30 13:33:01 +0700

    feat: update alert style custom Swal classes, icon, layout, and animations

--- src/services/alert.js
+++ src/services/alert.js
@@ -3,13 +3,20 @@ import Swal from "sweetalert2";
 export const alertSuccess = async (message, titles) => {
     return Swal.fire({
         imageUrl: '/icon-alert-success.png',
-        imageWidth: 200,
-        imageHeight: 200,
+        imageWidth: 120,
+        imageHeight: 120,
         title: titles,
         text: message,
         confirmButtonText: "Oke",
-    })
-}
+        customClass: {
+            popup: "my-alert-popup",
+            title: "my-alert-title",
+            htmlContainer: "my-alert-text",
+            confirmButton: "my-confirm-btn",
+        },
+        buttonsStyling: false,
+    });
+};
 
 export const alertError = async (message, titles) => {
     return Swal.fire({
@@ -34,12 +41,19 @@ export const alertConfirm = async (message, titles) => {
         title: titles,
         text: message,
         imageUrl: '/icon-alert-confirm.png',
-        imageWidth: 200,
-        imageHeight: 200,
+        imageWidth: 120,
+        imageHeight: 120,
         showCancelButton: true,
         confirmButtonColor: "#8A86D5",
         cancelButtonColor: "#8A86D5",
-        confirmButtonText: "Ya"
+        confirmButtonText: "Ya",
+        customClass: {
+            popup: "my-alert-popup",
+            title: "my-alert-title",
+            htmlContainer: "my-alert-text",
+            confirmButton: "my-confirm-btn",
+        },
+        buttonsStyling: false,
     }).then((result) => {
         if (result.isConfirmed) {
             Swal.fire({
@@ -47,8 +61,15 @@ export const alertConfirm = async (message, titles) => {
                 text: message,
                 confirmButtonText: "Tidak",
                 imageUrl: "https://unsplash.it/400/200",
-                imageWidth: 200,
-                imageHeight: 200,
+                imageWidth: 120,
+                imageHeight: 120,
+                customClass: {
+                    popup: "my-alert-popup",
+                    title: "my-alert-title",
+                    htmlContainer: "my-alert-text",
+                    confirmButton: "my-confirm-btn",
+                },
+                buttonsStyling: false,
             });
         }
     });
