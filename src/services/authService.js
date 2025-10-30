458d3c16c683d4776bc1670610b156b8661022b8
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-10-30 13:34:27 +0700

    feat: add Google OAuth login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,25 +1,40 @@
 import {API_PATH} from './api.js';
 
-export const userLogin = ({fullName, email, password}) =>
+export const userLogin = ({email, password}) =>
     fetch(`${API_PATH}/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
-        body: JSON.stringify({fullName, email, password}),
+        body: JSON.stringify({email, password}),
     });
 
-export const userRegister = async ({email, password}) => {
-    return await fetch(`${API_PATH}/auth/login`, {
+export const userRegister = async ({fullName, email, password}) => {
+    return await fetch(`${API_PATH}/auth/register`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
         body: JSON.stringify({
+            fullName,
             email,
             password
         }),
     });
+};
+
+export const UserAuthGoggle = ({email, password}) =>
+    fetch(`${API_PATH}/auth/google/login`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({email, password}),
+    });
+
+export const UserAuthGoogle = () => {
+    window.location.href = `${API_PATH}/auth/google/login`; //  Redirect browser ke endpoint Google OAuth
 };
