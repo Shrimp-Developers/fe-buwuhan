2e3dd23e2d9211abfdb913ee4b1c61e380deb843
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-11-03 13:23:34 +0700

    feat: add authentication service functions

--- src/context/useAuth.js
+++ src/context/useAuth.js
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
-import { AuthContext } from './AuthContext.jsx'
+import { AuthContext } from './AuthContext';
 
-export function useAuth() {
+export const useAuth = () => {
     const context = useContext(AuthContext);
+
     if (!context) {
         throw new Error('useAuth must be used within an AuthProvider');
     }
+
     return context;
-}
+};
