613f7f3fbbde961dc180ede8fa8a98b5e4312e11
hdibudihardoyo <hdibudihardoyo@gmail.com>
2025-11-07 10:01:16 +0700

    style: add h-screen sidebar mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ export default function Sidebar({ isOpen, onClose }) {
             {/* Mobile  */}
             {isOpen && (
                 <div
-                    className="fixed z-40 md:hidden h-full"
+                    className="fixed z-40 md:hidden h-screen"
                     onClick={onClose}
                 />
             )}
