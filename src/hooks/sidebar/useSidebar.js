e3a2b08877774f7b185e30eca801d7fdce6c1ebe
hdibudihardoyo <hdibudihardoyo@gmail.com>
2026-03-10 00:43:01 +0700

    style: format code clean

--- src/hooks/sidebar/useSidebar.js
+++ src/hooks/sidebar/useSidebar.js
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext.jsx";
-import { alertConfirm, alertSuccess } from "../../lib/sweeAlert.js";
+import { alertConfirm, alertSuccess } from "../../lib/sweetAlert.js";
 
 export default function useSidebar() {
   const { pathname } = useLocation();
