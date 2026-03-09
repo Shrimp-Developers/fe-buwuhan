1bcca61b63e49335fe9c18309aca95e15f889dd2
hdibudihardoyo <hdibudihardoyo@gmail.com>
2026-03-10 00:29:40 +0700

    feat: add export buwuhan excel API service and export hook with loading state

--- src/pages/BuwuhanList.jsx
+++ src/pages/BuwuhanList.jsx
@@ -1,9 +1,10 @@
 import { useState, useRef } from "react";
-import { ChevronDown, Search, MoreVertical } from "lucide-react";
+import { ChevronDown, Search, MoreVertical, Download } from "lucide-react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import useBuwuhanList from "../hooks/buwuhan/useBuwuhanList";
 import useDropdownOutside from "../hooks/buwuhan/useDropdownOutside";
 import DetailBuwuhan from "../components/DetailBuwuhan";
+import useBuwuhanExportData from "../hooks/buwuhan/useBuwuhanExportData"
 import {
   getCategoryLabelByName,
   getStatusLabel,
@@ -15,30 +16,14 @@ export default function BuwuhanList() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
-  const searchQuery = searchParams.get("search") || "";
+  const searchQuery = searchParams.get("search");
+  const category = searchParams.get("category");
+  const status = searchParams.get("status");
+
   const [searchValue, setSearchValue] = useState(searchQuery);
-
-  const {
-    data,
-    loading,
-    error,
-    category,
-    status,
-    handleCategoryChange,
-    handleChangeStatus,
-    pagination,
-    handlePrevPage,
-    handleNextPage,
-    handleChangeSize,
-    handleChangePage,
-    handleDeleteData,
-  } = useBuwuhanList(searchQuery);
-
   const [showCategory, setShowCategory] = useState(false);
   const [showStatus, setShowStatus] = useState(false);
-
   const [openAction, setOpenAction] = useState(null);
-
   const [detailModal, setDetailModal] = useState({
     isOpen: false,
     buwuhanId: null,
@@ -50,10 +35,43 @@ export default function BuwuhanList() {
   useDropdownOutside(categoryRef, setShowCategory);
   useDropdownOutside(statusRef, setShowStatus);
 
+  const { handleExportDataBuwuhan, isExporting } = useBuwuhanExportData()
+
+  const {
+    data,
+    loading,
+    error,
+    pagination,
+    handlePrevPage,
+    handleNextPage,
+    handleChangeSize,
+    handleChangePage,
+    handleDeleteData,
+  } = useBuwuhanList({ searchQuery, category, status });
+
+  const updateURL = (updates) => {
+    const params = new URLSearchParams(searchParams);
+    Object.entries(updates).forEach(([key, val]) => {
+      if (val) params.set(key, val);
+      else params.delete(key);
+    });
+    navigate(`/dashboard/list?${params.toString()}`);
+  };
+
   const handleSearch = (e) => {
     if (e.key === "Enter") {
-      navigate(`/dashboard/list?search=${searchValue}`);
+      updateURL({ search: searchValue, page: null });
     }
+  };
+
+  const handleCategoryChange = (val) => {
+    updateURL({ category: val, page: null });
+    setShowCategory(false);
+  };
+
+  const handleChangeStatus = (val) => {
+    updateURL({ status: val, page: null });
+    setShowStatus(false);
   };
 
   return (
@@ -81,7 +99,6 @@ export default function BuwuhanList() {
                 >
                   Semua Kategori
                 </button>
-
                 {CATEGORY_OPTIONS.map((opt) => (
                   <button
                     key={opt.value}
@@ -116,7 +133,6 @@ export default function BuwuhanList() {
                 >
                   Semua Status
                 </button>
-
                 {STATUS_OPTIONS.map((opt) => (
                   <button
                     key={opt.value}
@@ -129,6 +145,16 @@ export default function BuwuhanList() {
               </div>
             )}
           </div>
+
+          {/* Export Data */}
+          <button
+            className="flex items-center gap-2 border px-3 py-2 rounded-full text-xs bg-[#628141] text-[#FFFFFF] hover:bg-[#40513B] transition cursor-pointer"
+            disabled={isExporting}
+            onClick={handleExportDataBuwuhan}
+          >
+            <Download  size={16} />
+            {isExporting ? "Mengexport Data..." : "Export Excel"}
+          </button>
         </div>
 
         {/* Search */}
@@ -141,7 +167,6 @@ export default function BuwuhanList() {
             onKeyDown={handleSearch}
             className="pl-5 pr-10 py-2 w-full h-[40px] bg-white rounded-full text-sm text-gray-800 placeholder-gray-400 focus:outline-none shadow border border-gray-200"
           />
-
           <Search className="w-4 h-4 text-gray-400 absolute right-4 top-1/2 -translate-y-1/2" />
         </div>
       </div>
@@ -167,108 +192,103 @@ export default function BuwuhanList() {
             </div>
           ) : (
             <div className="border-1 rounded-2xl">
-            <div className="p-7">
-              <table className="w-full text-xs md:text-s">
-                <thead>
-                  <tr>
-                    <th className="p-2 text-left">Nama Laki-laki</th>
-                    <th className="p-2 text-left">Nama Perempuan</th>
-                    <th className="p-2 text-left">Kategori</th>
-                    <th className="p-2 text-left">Status</th>
-                    <th className="p-2 text-center">Aksi</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {data.map((row) => (
-                    <tr key={row.id} className="border-b">
-                      <td className="p-2">{row.nameMan}</td>
-                      <td className="p-2">{row.nameWoman}</td>
-                      <td className="p-2">
-                        {getCategoryLabelByName(row.categoryName)}
-                      </td>
-                      <td className="p-2">{getStatusLabel(row.status)}</td>
-
-                      <td className="p-2 text-center relative">
-                        {/* Desktop Actions */}
-                        <div className="hidden sm:flex justify-center gap-2">
-                          <button
-                            onClick={() => navigate(`/buwuhan/edit/${row.id}`)}
-                            className="px-3 py-1 border rounded-full text-xs"
-                          >
-                            Edit
-                          </button>
-
-                          <button
-                            onClick={() =>
-                              setDetailModal({
-                                isOpen: true,
-                                buwuhanId: row.id,
-                              })
-                            }
-                            className="px-3 py-1 bg-[#8A86D5] text-white rounded-full text-xs"
-                          >
-                            Detail
-                          </button>
-
-                          <button
-                            onClick={() => handleDeleteData(row.id)}
-                            className="px-3 py-1 bg-red-600 text-white rounded-full text-xs"
-                          >
-                            Hapus
-                          </button>
-                        </div>
-
-                        {/* Mobile Options */}
-                        <div className="sm:hidden flex justify-center">
-                          <button
-                            onClick={() =>
-                              setOpenAction(
-                                openAction === row.id ? null : row.id,
-                              )
-                            }
-                            className="p-2 rounded-full hover:bg-gray-200"
-                          >
-                            <MoreVertical size={18} />
-                          </button>
-
-                          {openAction === row.id && (
-                            <div className="absolute right-2 mt-2 w-32 bg-white border rounded-lg shadow-lg z-30">
-                              <button
-                                onClick={() =>
-                                  navigate(`/buwuhan/edit/${row.id}`)
-                                }
-                                className="w-full text-left px-3 py-2 text-xs hover:bg-gray-100"
-                              >
-                                Edit
-                              </button>
-
-                              <button
-                                onClick={() =>
-                                  setDetailModal({
-                                    isOpen: true,
-                                    buwuhanId: row.id,
-                                  })
-                                }
-                                className="w-full text-left px-3 py-2 text-xs hover:bg-gray-100"
-                              >
-                                Detail
-                              </button>
-
-                              <button
-                                onClick={() => handleDeleteData(row.id)}
-                                className="w-full text-left px-3 py-2 text-xs text-red-600 hover:bg-gray-100"
-                              >
-                                Hapus
-                              </button>
-                            </div>
-                          )}
-                        </div>
-                      </td>
+              <div className="py-3 px-5">
+                <table className="w-full text-xs md:text-s">
+                  <thead>
+                    <tr>
+                      <th className="p-2 text-left">Nama Laki-laki</th>
+                      <th className="p-2 text-left">Nama Perempuan</th>
+                      <th className="p-2 text-left">Kategori</th>
+                      <th className="p-2 text-left">Status</th>
+                      <th className="p-2 text-center">Aksi</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {data.map((row) => (
+                      <tr key={row.id} className="border-b">
+                        <td className="p-2">{row.nameMan}</td>
+                        <td className="p-2">{row.nameWoman}</td>
+                        <td className="p-2">
+                          {getCategoryLabelByName(row.categoryName)}
+                        </td>
+                        <td className="p-2">{getStatusLabel(row.status)}</td>
+                        <td className="p-2 text-center relative">
+                          {/* Desktop */}
+                          <div className="hidden sm:flex justify-center gap-2">
+                            <button
+                              onClick={() =>
+                                navigate(`/dashboard/edit/${row.id}`)
+                              }
+                              className="px-3 py-1 border rounded-full text-xs"
+                            >
+                              Edit
+                            </button>
+                            <button
+                              onClick={() =>
+                                setDetailModal({
+                                  isOpen: true,
+                                  buwuhanId: row.id,
+                                })
+                              }
+                              className="px-3 py-1 bg-[#8A86D5] text-white rounded-full text-xs"
+                            >
+                              Detail
+                            </button>
+                            <button
+                              onClick={() => handleDeleteData(row.id)}
+                              className="px-3 py-1 bg-red-600 text-white rounded-full text-xs"
+                            >
+                              Hapus
+                            </button>
+                          </div>
+
+                          {/* Mobile */}
+                          <div className="sm:hidden flex justify-center">
+                            <button
+                              onClick={() =>
+                                setOpenAction(
+                                  openAction === row.id ? null : row.id,
+                                )
+                              }
+                              className="p-2 rounded-full hover:bg-gray-200"
+                            >
+                              <MoreVertical size={18} />
+                            </button>
+                            {openAction === row.id && (
+                              <div className="absolute right-2 mt-2 w-32 bg-white border rounded-lg shadow-lg z-30">
+                                <button
+                                  onClick={() =>
+                                    navigate(`/dashboard/edit/${row.id}`)
+                                  }
+                                  className="w-full text-left px-3 py-2 text-xs hover:bg-gray-100"
+                                >
+                                  Edit
+                                </button>
+                                <button
+                                  onClick={() =>
+                                    setDetailModal({
+                                      isOpen: true,
+                                      buwuhanId: row.id,
+                                    })
+                                  }
+                                  className="w-full text-left px-3 py-2 text-xs hover:bg-gray-100"
+                                >
+                                  Detail
+                                </button>
+                                <button
+                                  onClick={() => handleDeleteData(row.id)}
+                                  className="w-full text-left px-3 py-2 text-xs text-red-600 hover:bg-gray-100"
+                                >
+                                  Hapus
+                                </button>
+                              </div>
+                            )}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           )}
@@ -277,13 +297,11 @@ export default function BuwuhanList() {
 
       {/* Pagination */}
       <div className="mt-6 flex flex-col lg:flex-row items-center justify-between gap-4 text-sm">
-        {/* Baris per halaman */}
         <div className="flex items-center gap-2 text-gray-600">
           <span>Baris per halaman</span>
-
           <div className="relative">
             <select
-              value={pagination.size}
+              value={pagination.currentSize}
               onChange={(e) => handleChangeSize(Number(e.target.value))}
               className="px-3 py-1 pr-6 border border-gray-300 rounded-full text-sm appearance-none bg-white focus:outline-none cursor-pointer"
             >
@@ -291,12 +309,10 @@ export default function BuwuhanList() {
               <option value={10}>10</option>
               <option value={20}>20</option>
             </select>
-
             <ChevronDown className="w-4 h-4 text-gray-600 absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none" />
           </div>
         </div>
 
-        {/* Navigation */}
         <div className="flex items-center gap-2">
           <button
             onClick={handlePrevPage}
@@ -305,26 +321,23 @@ export default function BuwuhanList() {
           >
             Sebelumnya
           </button>
-
-          {/* Page Numbers */}
           <div className="flex items-center gap-1">
             {Array.from({ length: pagination.totalPage }, (_, i) => i + 1).map(
-              (page) => (
+              (pg) => (
                 <button
-                  key={page}
-                  onClick={() => handleChangePage(page)}
+                  key={pg}
+                  onClick={() => handleChangePage(pg)}
                   className={`w-8 h-8 flex items-center justify-center rounded-lg font-medium transition ${
-                    pagination.currentPage === page
+                    pagination.currentPage === pg
                       ? "bg-[#8A86D5] text-white"
                       : "text-gray-600 hover:bg-gray-200"
                   }`}
                 >
-                  {page}
+                  {pg}
                 </button>
               ),
             )}
           </div>
-
           <button
             onClick={handleNextPage}
             disabled={pagination.currentPage >= pagination.totalPage}
@@ -334,22 +347,15 @@ export default function BuwuhanList() {
           </button>
         </div>
 
-        {/* Total */}
         <div className="text-gray-700">
           Total: <span className="font-semibold">{pagination.totalData}</span>
         </div>
       </div>
 
-      {/* Modal */}
       <DetailBuwuhan
         isOpen={detailModal.isOpen}
         buwuhanId={detailModal.buwuhanId}
-        onClose={() =>
-          setDetailModal({
-            isOpen: false,
-            buwuhanId: null,
-          })
-        }
+        onClose={() => setDetailModal({ isOpen: false, buwuhanId: null })}
       />
     </div>
   );
