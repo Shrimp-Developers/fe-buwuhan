eacd7f51aff56f2083c1adffe7644056b185b8ee
hdibudihardoyo <hdibudihardoyo@gmail.com>
2026-03-12 16:32:19 +0700

    feat: add axios instance with cookie support

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,23 +1,11 @@
 import axios from "axios";
+
 const API_BASE = import.meta.env.VITE_API_PATH;
 
 const AxiosInstance = axios.create({
   baseURL: API_BASE,
+  withCredentials: true, 
 });
 
-AxiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default AxiosInstance;
